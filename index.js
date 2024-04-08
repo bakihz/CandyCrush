0a74d5a68fe274e4376c315b8744c3dc2dd0b83c
bakihz <cmylmzfake@gmail.com>
2024-04-08 20:39:28 +0300

    f

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.body.onload = addElement;
+document.body.onload = addElement();
 
 function addElement() {
   const newDiv = document.createElement("div");
@@ -6,14 +6,18 @@ function addElement() {
   const newContent = document.createTextNode("hi there");
   newDiv.appendChild(newContent);
 
+  const parentDiv = document.getElementById("gameBox");
   const currentDiv = document.getElementById("div1");
 
-  document.body.insertBefore();
+  console.log("hi", newDiv);
+  parentDiv.insertBefore(newDiv, currentDiv);
   newDiv.style.width = "100px";
   newDiv.style.height = "100px";
-  newDiv.style.backgroundColor = "red";
+  newDiv.style.backgroundColor = "blue";
   newDiv.style.top = "0px";
   newDiv.style.left = "0px";
+  newDiv.style.display = "flex";
+  newDiv.innerHTML = "hey";
 }
 
 const candys = {};
