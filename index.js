f654d02bd361fe2f7ebeec394259d90b8b33bcb0
bakihz <huseyinfaruktopcu@gmail.com>
2024-04-11 13:10:37 +0300

    before table

--- index.js
+++ index.js
@@ -14,17 +14,18 @@ function addElement() {
   outerBox.appendChild(innerBox);
 
   outerBox.style.cssText = `
-   width:12.5%;
-   height: 100px;
+   width:87px;
+   height: 12.5vh;
    top:0px;
    left:0px;
+   background-color:gray;
    display:flex;
    justify-content:center;
    align-items:center;
   `;
   innerBox.style.cssText = `
-   width: 90px;
-   height:90px;
+   width:90%;
+   height:90%;
    background-color:white;
    display:flex;
   `;
@@ -35,5 +36,8 @@ addElement();
 addElement();
 addElement();
 addElement();
+addElement();
+addElement();
+
 
 const candys = {};
